55d83585e0b75e07515e36862dfb1907c66d59e0
ahSOLO <junmenghan@gmail.com>
2021-01-26 21:52:52 -0800

    begin setting up server

---
+++ server.js
@@ -0,0 +1,45 @@
+// Express Setup
+const PORT          = 8080;
+const express       = require("express");
+const server        = express();
+
+const bodyParser    = require("body-parser");
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(express.static("public"));
+
+// Game Boards:
+const board1 = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+];
+const board2 = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+];
+
+// Routes
+server.get("/game", function (req, res) {
+  res.json(board1, board2);
+});
+
+server.post("/game", function (req, res) {
+  board1 = req.body.board1;
+  board2 = req.body.board2;
+  res.json(board1, board2);
+});
